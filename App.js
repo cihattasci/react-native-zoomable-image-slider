e1925ad8038b3c38d6f15c126e70bfec13bcefa7
cihattasci <63815355+cihattasci@users.noreply.github.com>
2021-11-03 19:23:45 +0300

    moved to scrollview for image slider

--- App.js
+++ App.js
@@ -4,21 +4,28 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  TouchableHighlight,
+  findNodeHandle,
   Dimensions,
+  Text,
+  ScrollView,
   Platform,
 } from 'react-native';
-import Slider from '@react-native-community/slider';
+import ImageSlider from 'react-native-image-slider';
 import Modal from 'react-native-modal';
 import {
   gestureHandlerRootHOC,
   PinchGestureHandler,
 } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
+import PagerView from 'react-native-pager-view';
 
-const {height} = Dimensions.get('screen');
+const {width, height} = Dimensions.get('screen');
 
 export default function App() {
   const pinchRef = useRef();
+  const zoomRef = useRef();
+  const myScroll = useRef();
   const _baseScale = new Animated.Value(1);
   const _pinchScale = new Animated.Value(1);
   const _scale = Animated.multiply(_baseScale, _pinchScale);
@@ -30,9 +37,10 @@ export default function App() {
     'https://picsum.photos/200/300?grayscale',
     'https://picsum.photos/200/300/?blur',
   ];
-  const [image, setImage] = useState(imageArray[0]);
+
   const [index, setIndex] = useState(0);
   const [modal, setModal] = useState(false);
+  const [image, setImage] = useState(imageArray[0]);
 
   const pinchGestureEvent = Animated.event(
     [{nativeEvent: {scale: _pinchScale}}],
@@ -47,57 +55,62 @@ export default function App() {
     }
   };
 
-  const PinchImage = gestureHandlerRootHOC(() => (
+  const PinchImage = () => (
     <View style={styles.modalView}>
-      <View style={{flex: 5, alignItems: 'center', justifyContent: 'center'}}>
-        <PinchGestureHandler
-          ref={pinchRef}
-          minPointers={2}
-          onGestureEvent={pinchGestureEvent}
-          onHandlerStateChange={pinchStateChange}>
-          <Animated.Image
-            source={{uri: image}}
-            style={{width: 300, height: 300, transform: [{scale: _scale}]}}
-          />
-        </PinchGestureHandler>
-      </View>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Slider
-          style={{width: 200, height: 40}}
-          step={0.1}
-          minimumValue={1}
-          maximumValue={2}
-          maximumTrackTintColor="#000000"
-          thumbTintColor="red"
-          onValueChange={index => {
-            _baseScale.setValue(index);
-          }}
-        />
-      </View>
+      <ScrollView
+        horizontal
+        ref={myScroll}
+        initial={2}
+        maximumZoomScale={1.6}
+        minimumZoomScale={1.0}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        onScrollEndDrag={e => console.log(e._dispatchInstances.onTouchStart)}
+        style={{overflow: 'hidden', height: 300}}>
+        {imageArray.map((item, index) => {
+          return (
+            <Image
+              source={{uri: item}}
+              resizeMode={'contain'}
+              style={{width: 390, height: 300}}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
-  ));
+  );
 
   return (
     <View style={styles.main}>
-      <View style={{flex: 5, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={() => setModal(true)}>
-          <Image source={{uri: image}} style={styles.image} />
-        </TouchableOpacity>
-      </View>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Slider
-          style={{width: 200, height: 40}}
-          step={1}
-          minimumValue={0}
-          maximumValue={imageArray.length - 1}
-          maximumTrackTintColor="#000000"
-          thumbTintColor="red"
-          onValueChange={index => {
-            setImage(imageArray[index]);
-            setIndex(index);
-          }}
-          value={index}
-        />
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 250,
+          }}>
+          <ImageSlider
+            images={imageArray}
+            position={index}
+            onPositionChanged={index => {
+              setIndex(index);
+              setImage(imageArray[index]);
+            }}
+            customSlide={({index, item, style, width}) => (
+              <TouchableOpacity
+                onPress={() => {
+                  setModal(true);
+                  //myScroll.current.scrollTo({x: 100, y: 0});
+                }}>
+                <Image
+                  source={{uri: item}}
+                  resizeMode={'contain'}
+                  style={{width: 390, height: 300}}
+                />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       </View>
       <Modal
         style={styles.modal}
@@ -125,11 +138,69 @@ const styles = StyleSheet.create({
     margin: 0,
   },
   modalView: {
-    height: height * 0.77,
+    height: height,
     backgroundColor: '#f2f2f2',
     borderTopRightRadius: 15,
     borderTopLeftRadius: 15,
     alignItems: 'center',
     paddingBottom: Platform.OS === 'ios' ? height * 0.03 : 0,
+    paddingTop: 100,
+  },
+  pagerView: {
+    width: '100%',
+    height: 100,
   },
 });
+
+/**
+ * <ImageSlider
+        images={imageArray}
+        position={index}
+        style={{width: '100%', backgroundColor: 'white'}}
+        onPositionChanged={index => {
+          setIndex(index);
+        }}
+        customSlide={({index, item, style, width}) => (
+          <View
+            key={index}
+            style={{
+              width: 390,
+              height: 300,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <PinchGestureHandler
+              ref={pinchRef}
+              minPointers={2}
+              onGestureEvent={pinchGestureEvent}
+              onHandlerStateChange={pinchStateChange}>
+              <Animated.Image
+                source={{uri: imageArray[index]}}
+                style={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: 15,
+                  transform: [{scale: _scale}],
+                }}
+              />
+            </PinchGestureHandler>
+          </View>
+        )}
+      />
+ */
+
+/**
+  * <ScrollView horizontal>
+            {imageArray.map((item, index) => {
+              return (
+                <TouchableOpacity onPress={() => setModal(true)}>
+                  <Image
+                    source={{uri: item}}
+                    resizeMode={'contain'}
+                    style={{width: 390, height: 300}}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+  */
