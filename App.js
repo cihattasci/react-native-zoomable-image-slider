8cfe127c54ea9850bc98139f7ec879e32cdf8178
cihattasci <63815355+cihattasci@users.noreply.github.com>
2021-11-04 19:00:16 +0300

    fixed

--- App.js
+++ App.js
@@ -1,213 +1,336 @@
-import React, {useState, useRef} from 'react';
+import React, {Component} from 'react';
 import {
+  Animated,
+  Easing,
   View,
-  StyleSheet,
   Image,
-  TouchableOpacity,
-  TouchableHighlight,
-  findNodeHandle,
   Dimensions,
-  Text,
   ScrollView,
-  Platform,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
-import ImageSlider from 'react-native-image-slider';
-import Modal from 'react-native-modal';
-import {
-  gestureHandlerRootHOC,
-  PinchGestureHandler,
-} from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated';
-import PagerView from 'react-native-pager-view';
 
-const {width, height} = Dimensions.get('screen');
+const deviceWidth = Dimensions.get('window').width;
+const DISMISS_MODAL_THRESHOLD = 150;
 
-export default function App() {
-  const pinchRef = useRef();
-  const zoomRef = useRef();
-  const myScroll = useRef();
-  const _baseScale = new Animated.Value(1);
-  const _pinchScale = new Animated.Value(1);
-  const _scale = Animated.multiply(_baseScale, _pinchScale);
-  let _lastScale = 1;
-  const imageArray = [
-    'https://picsum.photos/200',
-    'https://picsum.photos/id/237/200/300',
-    'https://picsum.photos/seed/picsum/200/300',
-    'https://picsum.photos/200/300?grayscale',
-    'https://picsum.photos/200/300/?blur',
-  ];
-  const colors = ['red', 'yellow', 'green', 'black', 'blue'];
-  const [index, setIndex] = useState(0);
-  const [modal, setModal] = useState(false);
-  const [image, setImage] = useState(imageArray[0]);
+const images = [
+  'https://picsum.photos/200',
+  'https://picsum.photos/id/237/200/300',
+  'https://picsum.photos/seed/picsum/200/300',
+  'https://picsum.photos/200/300?grayscale',
+  'https://picsum.photos/200/300/?blur',
+];
 
-  const pinchGestureEvent = Animated.event(
-    [{nativeEvent: {scale: _pinchScale}}],
-    {useNativeDriver: true},
-  );
+class ZoomView extends Component {
+  static defaultProps = {
+    doAnimateZoomReset: false,
+    maximumZoomScale: 2,
+    minimumZoomScale: 1,
+    zoomed: false,
+    zoomEnabled: false,
+    zoomHeight: 219,
+    zoomWidth: deviceWidth,
+  };
 
-  const pinchStateChange = event => {
-    if (event.nativeEvent.oldState === 4) {
-      _lastScale *= event.nativeEvent.scale;
-      _baseScale.setValue(_lastScale);
-      _pinchScale.setValue(1);
+  state = {
+    startY: null,
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.zoomed === true && this.props.zoomed === false) {
+      this.handleResetZoomScale();
+    }
+  }
+
+  setZoomRef = node => {
+    if (node) {
+      this.zoomRef = node;
+      this.scrollResponderRef = this.zoomRef.getScrollResponder();
+
+      this.scrollResponderRef.scrollResponderHandleTouchStart = event => {
+        const isZoom = event.nativeEvent.touches.length > 1 ? true : false;
+
+        if (!this.props.zoomed) {
+          this.setState({startY: event.nativeEvent.locationY});
+        }
+
+        if (isZoom) {
+          if (!this.props.zoomed) {
+            this.props.onZoomed();
+          }
+        }
+      };
+
+      this.scrollResponderRef.scrollResponderHandleTouchEnd = event => {
+        console.log('end');
+        if (this.props.zoomed) {
+          this.imageRef.measure((ox, oy, width) => {
+            if (width <= this.props.zoomWidth) {
+              this.props.onZoomExit();
+              return;
+            } else {
+              return;
+            }
+          });
+        } else {
+          const isZoom = event.nativeEvent.touches.length > 1 ? true : false;
+
+          if (!isZoom) {
+            const currentY = event.nativeEvent.locationY;
+            const scrollYDistance = Math.abs(this.state.startY - currentY);
+            if (scrollYDistance > DISMISS_MODAL_THRESHOLD) {
+              this.props.onZoomClosePress();
+            }
+          }
+        }
+      };
     }
   };
 
-  const PinchImage = () => (
-    <View style={styles.modalView}>
+  setImageRef = node => {
+    if (node) {
+      this.imageRef = node;
+    }
+  };
+
+  handleZoomViewPress = () => {
+    this.props.setModal();
+    if (!this.props.zoomEnabled) {
+      this.props.onZoomEnabled();
+    } else {
+      if (this.props.zoomed) {
+        this.handleResetZoomScale();
+        this.props.onZoomClosePress();
+      }
+    }
+  };
+
+  handleResetZoomScale = () => {
+    this.scrollResponderRef.scrollResponderZoomTo({
+      x: 0,
+      y: 0,
+      width: this.props.zoomWidth,
+      height: this.props.zoomHeight,
+      animated: true,
+    });
+  };
+
+  render() {
+    return (
       <ScrollView
-        horizontal
-        ref={myScroll}
-        initial={2}
-        maximumZoomScale={1.6}
-        minimumZoomScale={1.0}
+        contentContainerStyle={{alignItems: 'center', justifyContent: 'center'}}
+        centerContent
+        maximumZoomScale={
+          this.props.zoomEnabled ? this.props.maximumZoomScale : 1
+        }
+        minimumZoomScale={this.props.minimumZoomScale}
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
-        //onScrollEndDrag={e => console.log(e._dispatchInstances.onTouchStart)}
-        style={{overflow: 'hidden', height: 300}}>
-        {imageArray.map((item, index) => {
-          return (
-            <View
-              style={{
-                backgroundColor: colors[index],
-                minWidth: 400,
-                maxHeight: 340,
-              }}>
-              <Image
-                source={{uri: item}}
-                resizeMode={'contain'}
-                style={{width: 390, height: 300}}
-              />
-            </View>
-          );
-        })}
-      </ScrollView>
-    </View>
-  );
-
-  return (
-    <View style={styles.main}>
-      <View style={{flex: 1}}>
-        <View
-          style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 250,
-          }}>
-          <ImageSlider
-            images={imageArray}
-            position={index}
-            onPositionChanged={index => {
-              setIndex(index);
-              setImage(imageArray[index]);
+        ref={this.setZoomRef}
+        scrollEnabled={this.props.zoomEnabled}
+        scrollEventThrottle={10}
+        style={{
+          overflow: 'visible',
+        }}>
+        <TouchableOpacity
+          onPress={this.handleZoomViewPress}
+          flexGrow={1}
+          flex={1}>
+          <Image
+            source={{uri: this.props.source}}
+            resizeMode="contain"
+            style={{
+              height: 219,
+              width: deviceWidth,
             }}
-            customSlide={({index, item, style, width}) => (
-              <TouchableOpacity
-                onPress={() => {
-                  setModal(true);
-                  //myScroll.current.scrollTo({x: 100, y: 0});
-                }}>
-                <Image
-                  source={{uri: item}}
-                  resizeMode={'contain'}
-                  style={{width: 390, height: 300}}
-                />
-              </TouchableOpacity>
-            )}
+            ref={this.setImageRef}
           />
-        </View>
-      </View>
-      <Modal
-        style={styles.modal}
-        onBackdropPress={() => setModal(false)}
-        isVisible={modal}>
-        <PinchImage />
-      </Modal>
-    </View>
-  );
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-  },
-  image: {
-    width: 300,
-    height: 300,
-  },
-  modal: {
-    bottom: Platform.OS === 'ios' ? -height * 0.25 : -height * 0.15,
-    justifyContent: 'flex-end',
-    margin: 0,
-  },
-  modalView: {
-    height: height,
-    backgroundColor: '#f2f2f2',
-    borderTopRightRadius: 15,
-    borderTopLeftRadius: 15,
-    alignItems: 'center',
-    paddingBottom: Platform.OS === 'ios' ? height * 0.03 : 0,
-    paddingTop: 100,
-  },
-  pagerView: {
-    width: '100%',
-    height: 100,
-  },
-});
+export default class App extends Component {
+  animZoomVal = new Animated.Value(0);
 
-/**
- * <ImageSlider
-        images={imageArray}
-        position={index}
-        style={{width: '100%', backgroundColor: 'white'}}
-        onPositionChanged={index => {
-          setIndex(index);
-        }}
-        customSlide={({index, item, style, width}) => (
-          <View
-            key={index}
+  animInverseZoomVal = this.animZoomVal.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  });
+
+  animValUpTiming = Animated.timing(this.animZoomVal, {
+    toValue: 1,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+  animValDownTiming = Animated.timing(this.animZoomVal, {
+    toValue: 0,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+  animTranslateY = this.animZoomVal.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 220],
+  });
+
+  animScrollBarOpacityVal = new Animated.Value(1);
+  animScrollOpacityUpTiming = Animated.timing(this.animScrollBarOpacityVal, {
+    toValue: 1,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+  animScrollOpacityDownTiming = Animated.timing(this.animScrollBarOpacityVal, {
+    toValue: 0,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+
+  animCloseOpacityVal = new Animated.Value(0);
+  animCloseOpacityUpTiming = Animated.timing(this.animCloseOpacityVal, {
+    toValue: 1,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+  animCloseOpacityDownTiming = Animated.timing(this.animCloseOpacityVal, {
+    toValue: 0,
+    duraton: 300,
+    easing: Easing.inOut(Easing.quad),
+  });
+
+  animScrollXVal = new Animated.Value(0);
+
+  scrollXVal = this.animScrollXVal.interpolate({
+    inputRange: [0, deviceWidth * (images.length - 1)],
+    outputRange: [0, (deviceWidth / images.length) * (images.length - 1)],
+    extrapolate: 'clamp',
+  });
+
+  state = {
+    zoomEnabled: false,
+    zoomed: false,
+    modal: false,
+  };
+
+  handleZoomEnabled = () => {
+    this.setState({zoomEnabled: true});
+    this.animValUpTiming.start();
+    this.animCloseOpacityUpTiming.start();
+  };
+
+  handleZoomed = () => {
+    this.setState({zoomed: true});
+    this.animScrollOpacityDownTiming.start();
+    this.animCloseOpacityDownTiming.start();
+  };
+
+  handleZoomClosePress = () => {
+    if (this.state.zoomEnabled) {
+      this.animScrollOpacityUpTiming.start();
+      this.setState({zoomEnabled: false, zoomed: false});
+      this.animValDownTiming.start();
+      this.animCloseOpacityDownTiming.start();
+    }
+  };
+
+  handleZoomExit = () => {
+    if (this.state.zoomed) {
+      this.setState({zoomed: false, zoomEnabled: true});
+      this.animScrollOpacityUpTiming.start();
+      this.animCloseOpacityUpTiming.start();
+    }
+  };
+
+  renderZoomView = (image, i) => {
+    return (
+      <ZoomView
+        key={i}
+        source={image}
+        zoomEnabled={this.state.zoomEnabled}
+        zoomed={this.state.zoomed}
+        onZoomEnabled={this.handleZoomEnabled}
+        onZoomClosePress={this.handleZoomClosePress}
+        onZoomExit={this.handleZoomExit}
+        onZoomed={this.handleZoomed}
+        translateY={this.animTranslateY}
+        index={i}
+        setModal={() => this.setState({modal: !this.state.modal})}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexGrow: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          marginTop: 40,
+        }}>
+        <Animated.View
+          style={{
+            transform: [
+              {
+                translateY:
+                  this.state.modal === false
+                    ? this.animZoomVal.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 220],
+                      })
+                    : this.animZoomVal.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 1],
+                      }),
+              },
+            ],
+            height: 219,
+            zIndex: 10,
+          }}>
+          <Animated.ScrollView
+            grow
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={10}
+            onScroll={Animated.event([
+              {nativeEvent: {contentOffset: {x: this.animScrollXVal}}},
+            ])}
+            scrollEnabled={!this.state.zoomed}
             style={{
-              width: 390,
-              height: 300,
-              alignItems: 'center',
-              justifyContent: 'center',
+              overflow: 'visible',
             }}>
-            <PinchGestureHandler
-              ref={pinchRef}
-              minPointers={2}
-              onGestureEvent={pinchGestureEvent}
-              onHandlerStateChange={pinchStateChange}>
-              <Animated.Image
-                source={{uri: imageArray[index]}}
+            {images.map((image, i) => {
+              return this.renderZoomView(image, i);
+            })}
+          </Animated.ScrollView>
+          <Animated.View
+            style={{
+              width: deviceWidth,
+              height: 5,
+            }}>
+            <Animated.View
+              style={{
+                backgroundColor: '#E5E5E5',
+                opacity: this.animScrollBarOpacityVal,
+              }}>
+              <Animated.View
                 style={{
-                  width: 200,
-                  height: 200,
-                  borderRadius: 15,
-                  transform: [{scale: _scale}],
+                  backgroundColor: '#111111',
+                  width: deviceWidth / images.length,
+                  height: 5,
+                  transform: [
+                    {
+                      translateX: this.scrollXVal,
+                    },
+                  ],
                 }}
               />
-            </PinchGestureHandler>
-          </View>
-        )}
-      />
- */
-
-/**
-  * <ScrollView horizontal>
-            {imageArray.map((item, index) => {
-              return (
-                <TouchableOpacity onPress={() => setModal(true)}>
-                  <Image
-                    source={{uri: item}}
-                    resizeMode={'contain'}
-                    style={{width: 390, height: 300}}
-                  />
-                </TouchableOpacity>
-              );
-            })}
-          </ScrollView>
-  */
+            </Animated.View>
+          </Animated.View>
+        </Animated.View>
+      </View>
+    );
+  }
+}
---
+++ backup.js
@@ -0,0 +1,213 @@
+/*import React, {useState, useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+  findNodeHandle,
+  Dimensions,
+  Text,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import ImageSlider from 'react-native-image-slider';
+import Modal from 'react-native-modal';
+import {
+  gestureHandlerRootHOC,
+  PinchGestureHandler,
+} from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+import PagerView from 'react-native-pager-view';
+
+const {width, height} = Dimensions.get('screen');
+
+export default function App() {
+  const pinchRef = useRef();
+  const zoomRef = useRef();
+  const myScroll = useRef();
+  const _baseScale = new Animated.Value(1);
+  const _pinchScale = new Animated.Value(1);
+  const _scale = Animated.multiply(_baseScale, _pinchScale);
+  let _lastScale = 1;
+  const imageArray = [
+    'https://picsum.photos/200',
+    'https://picsum.photos/id/237/200/300',
+    'https://picsum.photos/seed/picsum/200/300',
+    'https://picsum.photos/200/300?grayscale',
+    'https://picsum.photos/200/300/?blur',
+  ];
+  const colors = ['red', 'yellow', 'green', 'black', 'blue'];
+  const [index, setIndex] = useState(0);
+  const [modal, setModal] = useState(false);
+  const [image, setImage] = useState(imageArray[0]);
+
+  const pinchGestureEvent = Animated.event(
+    [{nativeEvent: {scale: _pinchScale}}],
+    {useNativeDriver: true},
+  );
+
+  const pinchStateChange = event => {
+    if (event.nativeEvent.oldState === 4) {
+      _lastScale *= event.nativeEvent.scale;
+      _baseScale.setValue(_lastScale);
+      _pinchScale.setValue(1);
+    }
+  };
+
+  const PinchImage = () => (
+    <View style={styles.modalView}>
+      <ScrollView
+        horizontal
+        ref={myScroll}
+        maximumZoomScale={1.6}
+        minimumZoomScale={1.0}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        //onScrollEndDrag={e => console.log(e._dispatchInstances.onTouchStart)}
+        style={{overflow: 'hidden', height: 300}}>
+        {imageArray.map((item, index) => {
+          return (
+            <View
+              style={{
+                backgroundColor: colors[index],
+                minWidth: 400,
+                maxHeight: 340,
+              }}>
+              <Image
+                source={{uri: item}}
+                resizeMode={'contain'}
+                style={{width: 390, height: 300}}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+
+  return (
+    <View style={styles.main}>
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 250,
+          }}>
+          <ImageSlider
+            images={imageArray}
+            position={index}
+            onPositionChanged={index => {
+              setIndex(index);
+              setImage(imageArray[index]);
+            }}
+            customSlide={({index, item, style, width}) => (
+              <TouchableOpacity
+                onPress={() => {
+                  setModal(true);
+                  //myScroll.current.scrollTo({x: 100, y: 0});
+                }}>
+                <Image
+                  source={{uri: item}}
+                  resizeMode={'contain'}
+                  style={{width: 390, height: 300}}
+                />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </View>
+      <Modal
+        style={styles.modal}
+        onBackdropPress={() => setModal(false)}
+        isVisible={modal}>
+        <PinchImage />
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  modal: {
+    bottom: Platform.OS === 'ios' ? -height * 0.25 : -height * 0.15,
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalView: {
+    height: height,
+    backgroundColor: '#f2f2f2',
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    alignItems: 'center',
+    paddingBottom: Platform.OS === 'ios' ? height * 0.03 : 0,
+    paddingTop: 100,
+  },
+  pagerView: {
+    width: '100%',
+    height: 100,
+  },
+});
+*/
+
+/**
+ * <ImageSlider
+        images={imageArray}
+        position={index}
+        style={{width: '100%', backgroundColor: 'white'}}
+        onPositionChanged={index => {
+          setIndex(index);
+        }}
+        customSlide={({index, item, style, width}) => (
+          <View
+            key={index}
+            style={{
+              width: 390,
+              height: 300,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <PinchGestureHandler
+              ref={pinchRef}
+              minPointers={2}
+              onGestureEvent={pinchGestureEvent}
+              onHandlerStateChange={pinchStateChange}>
+              <Animated.Image
+                source={{uri: imageArray[index]}}
+                style={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: 15,
+                  transform: [{scale: _scale}],
+                }}
+              />
+            </PinchGestureHandler>
+          </View>
+        )}
+      />
+ */
+
+/**
+  * <ScrollView horizontal>
+            {imageArray.map((item, index) => {
+              return (
+                <TouchableOpacity onPress={() => setModal(true)}>
+                  <Image
+                    source={{uri: item}}
+                    resizeMode={'contain'}
+                    style={{width: 390, height: 300}}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+  */
